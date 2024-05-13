259abb34c5bf2c498365ead912a887543725a1f6
Ioan Zaharia <zaharia.ioan@gmail.com>
2024-05-13 16:16:06 +0000

    add code to calculate the correct and incorrect score

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,10 +43,12 @@ function checkAnswer() {
 
   if (isCorrect) {
     alert("Hey! you got it right! :D");
+    incrementScore();
   } else {
     alert(
       `Awwww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`
     );
+    incrementWrongAnswer();
   }
 
   runGame(calculatedAnswer[1]);
@@ -69,9 +71,21 @@ function calculateCorrectAnswer() {
   }
 }
 
-function incrementScore() {}
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+function incrementScore() {
+  let oldScore = parseInt(document.getElementById("score").innerText);
+  document.getElementById("score").innerText = ++oldScore;
+}
 
-function incrementWrongAnswer() {}
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
+function incrementWrongAnswer() {
+  let oldScore = parseInt(document.getElementById("incorrect").innerText);
+  document.getElementById("incorrect").innerText = ++oldScore;
+}
 
 function displayAdditionQuestion(operand1, operand2) {
   document.getElementById("operand1").textContent = operand1;
